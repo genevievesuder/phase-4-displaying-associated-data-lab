263ea3543eeebf4084c8d16c205a808549627d02
Genevieve Suder <genevievesuder@gmail.com>
2023-02-02 10:37:42 -0800

    commit tree 9375678bf20b5e2164e113ccf429a8bca5d985e0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items, include: :user, status: :ok
+    end
+
+
+    #^This method is sending back all of the items and including their associated users
+    #Item belongs_to :user
+    #One to many relationship
+
+
+    # def
+    # items = Item.all
+    # render json: items, only: [:description, :name, :price],
+    # include: [user: {only: [:username, :city]}] status: :ok
+    # end
+
+    #^This method is Grabbing all Items -but only the Item's descriptions, names, and prices
+    #Including the user associated with THAT item -but only the users usernames and cities
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items, status: :ok
+    end
 end
+
+#This method is sending back a specific user (by id) and their associated items 
+#Users has_many :items
+#One to many relationship
